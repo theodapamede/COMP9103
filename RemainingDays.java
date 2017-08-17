c2645165bdfeedbfacf7c2619afe0122f563bbfa
Theo Dapamede <theodapamede@users.noreply.github.com>
2017-08-17 14:47:24 +1000

    Update RemainingDays.java

--- RemainingDays.java
+++ RemainingDays.java
@@ -1,20 +1,46 @@
 /*
- * This program is to calculate the remaining days
- * for submission.
- * Days: Monday = 1 ... Sunday = 7
+ * This program is to calculate the day of submission.
+ * By Theodorus Dapamede (SID. 470239731)
  */
 public class RemainingDays {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		final int TODAY = 3;
-		final int DUE = 10;
-		int due_day=TODAY+DUE%7;
-		if (due_day > 7){
-			System.out.println(due_day-7);
+		final String TODAY = "Wednesday"; //day of today; case-insensitive
+		final int DUE = 10; //number of days remaining
+		int dueday=999; //initializing an arbitrary default number
+		
+		if (TODAY.equalsIgnoreCase("Monday"))
+			dueday=1;
+		else if (TODAY.equalsIgnoreCase("Tuesday"))
+			dueday=2;
+		else if (TODAY.equalsIgnoreCase("Wednesday"))
+			dueday=3;
+		else if (TODAY.equalsIgnoreCase("Thursday"))
+			dueday=4;
+		else if (TODAY.equalsIgnoreCase("Friday"))
+			dueday=5;
+		else if (TODAY.equalsIgnoreCase("Saturday"))
+			dueday=6;
+		else if (TODAY.equalsIgnoreCase("Sunday"))
+			dueday=7;
+		else
+			System.out.println("You have input an invalid day for today!");
+		
+		dueday = dueday+DUE%7;//calculating the new dueday
+		
+		if (dueday > 7)
+			dueday=dueday-7;
+		
+		switch (dueday){
+		case 1: System.out.print("The submission is due in "+DUE+" days, which is a Monday.");break;
+		case 2: System.out.print("The submission is due in "+DUE+" days, which is a Tuesday.");break;
+		case 3: System.out.print("The submission is due in "+DUE+" days, which is a Wednesday.");break;
+		case 4: System.out.print("The submission is due in "+DUE+" days, which is a Thursday.");break;
+		case 5: System.out.print("The submission is due in "+DUE+" days, which is a Friday.");break;
+		case 6: System.out.print("The submission is due in "+DUE+" days, which is a Saturday.");break;
+		case 7: System.out.print("The submission is due in "+DUE+" days, which is a Sunday.");break;
+		default: System.out.print("Please enter the correct values.");break;
 		}
-		else{
-			System.out.println(due_day);
-		}
+		
 	}
 }
